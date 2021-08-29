3b6e46e9e3442b8113de096fbf79c0d32a7d4e11
Dawood <83209168+14685-562@users.noreply.github.com>
2021-08-30 00:25:56 +0100

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -5,6 +5,12 @@ from discord.ext import commands
 class help(commands.Cog):
     def __init__(self, client):
         self.client = client
+    
+    @commands.group(aliases=['h'], invoke_without_command=True)
+    async def help(self, ctx, *helptype):
+        cord.Embed(color=random.choice(embedcolours), title='Help'))
+        HelpEmbed = discord.Embed(color=random.choice(embedcolours), title='Help'))
+
 
 def setup(client):
     client.add_cog(help(client))
