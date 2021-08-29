fdeda8b9ab70c76f591d1c5fd65745bdef81ddb1
Dawood <83209168+14685-562@users.noreply.github.com>
2021-08-30 00:11:11 +0100

    Update errorhandling.py

--- cogs/errorhandling.py
+++ cogs/errorhandling.py
@@ -2,3 +2,10 @@ import discord
 from discord.ext import commands
 
 MissingRequiredArgument
+
+class errorhandling(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+def setup(client):
+    client.add_cog(errorhandling(client))
