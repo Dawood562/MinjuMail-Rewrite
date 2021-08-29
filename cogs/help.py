c9f13288e73cc8e5a06ff018254f1de637370d5c
Dawood <83209168+14685-562@users.noreply.github.com>
2021-08-29 20:57:57 +0100

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -1,3 +1,10 @@
 import discord
 from datetime import datetime
 from discord.ext import commands
+
+class help(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+def setup(client):
+    client.add_cog(help(client))
