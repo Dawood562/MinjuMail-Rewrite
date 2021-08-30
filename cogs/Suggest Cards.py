352f54ace2f3ff51b51a727e871f31ef03c8151a
Dawood <83209168+14685-562@users.noreply.github.com>
2021-08-30 21:56:01 +0100

    Update Suggest Cards.py

--- cogs/Suggest Cards.py
+++ cogs/Suggest Cards.py
@@ -4,13 +4,13 @@ from discord.ext import commands
 import random
 embedcolours = [discord.Color.from_rgb(217,89,140), discord.Color.from_rgb(241,210,231), discord.Color.from_rgb(243,170,81), discord.Color.from_rgb(252,246,149), discord.Color.from_rgb(86,122,206), discord.Color.from_rgb(183,211,233), discord.Color.from_rgb(187,176,220), discord.Color.from_rgb(219,112,108), discord.Color.from_rgb(241,195,170), discord.Color.from_rgb(206,229,213), discord.Color.from_rgb(254,254,254), discord.Color.from_rgb(167,224,225)]
 
-class Suggest_Cards(commands.Cog, name='🗨️ Suggest Cards', case_insensitive=True):
+class Suggest_Cards(commands.Cog, name='🗨️ Suggest Cards'):
     """Suggest cards to be added to the Minju bot!"""
     def __init__(self, client):
         self.client = client
 
         
-    @commands.group(invoke_without_command=True, aliases=['s'], description='Suggest cards to be added to the Minju bot! All subcommands have a 2 minute cooldown.')
+    @commands.group(invoke_without_command=True, aliases=['s'], description='Suggest cards to be added to the Minju bot! All subcommands have a 2 minute cooldown.', case_insensitive=True)
     @discord.ext.commands.dm_only()
     async def suggest(self, ctx):
         await ctx.send(embed=discord.Embed(color=random.choice(embedcolours), title='Suggestion', description='Missing arguments. Please use one of `group`, `drama`, or `soloist`.'))
