f768c0f2aaa38d157ad5c3fdcc53eba9d6f01650
Dawood <83209168+14685-562@users.noreply.github.com>
2021-08-30 23:21:28 +0100

    Update Misc Commands.py

--- cogs/Misc Commands.py
+++ cogs/Misc Commands.py
@@ -15,11 +15,12 @@ class Misc_Commands(commands.Cog, name='❓ Miscellaneous Commands'):
         await ctx.send(f'This bot was coded by Snow (**DaSnow562#0562**). If it breaks, get a Minju\'s Manager to reach out to him to fix it!')
 
 
-    @commands.command(description='Bot information')
+    @commands.command(description='Bot information.')
     async def bot(self, ctx):
         embed = discord.Embed(title='Bot Information')
         embed.set_author(name='Bot', icon_url=self.context.author.avatar_url)
-        embed.add_field(name='Title', value='Something', inline=True)
+        embed.add_field(name='Latency', value=f'{round(self.client.latency*1000, 1)}ms', inline=True)
+        embed.add_field(name='Members in Minju Support', value=f'{client.get_guild(714926445595721820).member_count}', inline=True)
         await ctx.send(embed=embed)
 
 def setup(client):
