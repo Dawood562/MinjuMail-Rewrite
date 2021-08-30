4fccd861cfa8080544db34240a14e92dab3bc961
Dawood <83209168+14685-562@users.noreply.github.com>
2021-08-30 19:18:27 +0100

    Update Reporting.py

--- cogs/Reporting.py
+++ cogs/Reporting.py
@@ -2,10 +2,15 @@ import discord
 from datetime import datetime
 from discord.ext import commands
 
-class Reporting(commands.Cog):
+class Reporting(commands.Cog, name='🗨️ Reporting'):
     """Report bugs, players, and scams!"""
     def __init__(self, client):
         self.client = client
+
+
+    @commands.command()
+    async def report(self, ctx):
+        await ctx.send('Reporting has begun. From scratch. I hate my life.')
 
 def setup(client):
     client.add_cog(Reporting(client))
