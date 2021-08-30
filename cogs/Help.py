1fc5f7a1e5817f7a32bc60d8e518a0ba40581eec
Dawood <83209168+14685-562@users.noreply.github.com>
2021-08-30 21:45:22 +0100

    Update Help.py

--- cogs/Help.py
+++ cogs/Help.py
@@ -45,7 +45,7 @@ class MyHelp(commands.HelpCommand):
         command_signatures = [self.get_command_signature(command) for command in group.commands]
         embed = discord.Embed(title=f'Help for {group}', description=f'**{group.description}**', color=random.choice(embedcolours))
         for i in range(len(scmdname)):
-            embed.add_field(name=f'{scmdname[i]}', value=f"{scmddesc[i]}", inline=True)
+            embed.add_field(name=f'{scmdname[i].capitalize()}', value=f"{scmddesc[i]}", inline=True)
         embed.set_author(name='Help', icon_url=self.context.author.avatar_url)
         await self.get_destination().send(embed=embed)
 
