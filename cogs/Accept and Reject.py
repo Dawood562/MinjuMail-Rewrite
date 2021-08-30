9c47579142d16022f1c85221aaf59bbf862c0b2c
Dawood <83209168+14685-562@users.noreply.github.com>
2021-08-30 12:47:07 +0100

    Update and rename acceptreject.py to Accept and Reject.py

--- cogs/Accept and Reject.py
+++ cogs/Accept and Reject.py
@@ -6,7 +6,7 @@ import random
 embedcolours = [discord.Color.from_rgb(217,89,140), discord.Color.from_rgb(241,210,231), discord.Color.from_rgb(243,170,81), discord.Color.from_rgb(252,246,149), discord.Color.from_rgb(86,122,206), discord.Color.from_rgb(183,211,233), discord.Color.from_rgb(187,176,220), discord.Color.from_rgb(219,112,108), discord.Color.from_rgb(241,195,170), discord.Color.from_rgb(206,229,213), discord.Color.from_rgb(254,254,254), discord.Color.from_rgb(167,224,225)]
 staffids = [389897179701182465, 221188745414574080, 303901339891531779, 257900648618655746, 193962293342502912, 364045258004365312, 297278578435948545]
 
-class acceptreject(commands.Cog):
+class Accept_and_Reject(commands.Cog):
     def __init__(self, client):
         self.client = client
 
@@ -120,4 +120,4 @@ class acceptreject(commands.Cog):
             await ctx.send(f'You need to be staff to use this {ctx.author.mention}!')
 
 def setup(client):
-    client.add_cog(acceptreject(client))
+    client.add_cog(Accept_and_Reject(client))
