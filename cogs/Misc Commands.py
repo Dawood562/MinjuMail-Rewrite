c77937bc7d9493fd169b51739c686f19f218481c
Dawood <83209168+14685-562@users.noreply.github.com>
2021-08-30 23:41:48 +0100

    Update Misc Commands.py

--- cogs/Misc Commands.py
+++ cogs/Misc Commands.py
@@ -21,7 +21,18 @@ class Misc_Commands(commands.Cog, name='❓ Miscellaneous Commands'):
         embed.set_author(name='Bot', icon_url=self.context.author.avatar_url)
         embed.add_field(name='Latency', value=f'{round(self.client.latency*1000, 1)}ms', inline=True)
         embed.add_field(name='Members in Minju Support', value=f'{client.get_guild(714926445595721820).member_count}', inline=True)
+
+        delta_uptime = datetime.utcnow() - bot.launch_time
+        hours, remainder = divmod(int(delta_uptime.total_seconds()), 3600)
+        minutes, seconds = divmod(remainder, 60)
+        days, hours = divmod(hours, 24)
+        await ctx.send(f"{days}d, {hours}h, {minutes}m, {seconds}s")
+        uptime = f'{days}d, {hours}h, {minutes}m, {seconds}s'
+        embed.add_field(name='Uptime', value=f'{uptime}', inline=True)
+
         await ctx.send(embed=embed)
+
+
 
 def setup(client):
     client.add_cog(Misc_Commands(client))
