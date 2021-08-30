0c62505fbd7b72cc561ba343c20b1591b9fa190e
Dawood <83209168+14685-562@users.noreply.github.com>
2021-08-30 23:16:00 +0100

    Update Misc Commands.py

--- cogs/Misc Commands.py
+++ cogs/Misc Commands.py
@@ -6,13 +6,21 @@ class Misc_Commands(commands.Cog, name='❓ Miscellaneous Commands'):
     def __init__(self, client):
         self.client = client
 
-    @commands.command()
+    @commands.command(description='Check the latency of the bot.')
     async def ping(self, ctx):
         await ctx.send(f'**Latency:** {round(self.client.latency*1000, 1)}ms')
 
-    @commands.command()
+    @commands.command(description='Snow.')
     async def snow(self, ctx):
         await ctx.send(f'This bot was coded by Snow (**DaSnow562#0562**). If it breaks, get a Minju\'s Manager to reach out to him to fix it!')
+
+
+    @commands.command(description='Bot information')
+    async def bot(self, ctx):
+        embed = discord.Embed(title='Bot Information')
+        embed.set_author(name='Bot', icon_url=self.context.author.avatar_url)
+        embed.add_field(name='Title', value='Something', inline=True)
+        await ctx.send(embed=embed)
 
 def setup(client):
     client.add_cog(Misc_Commands(client))
