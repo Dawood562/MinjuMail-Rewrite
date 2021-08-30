ee7f6c119b611c7d388de863ac5d332750b18ee0
Dawood <83209168+14685-562@users.noreply.github.com>
2021-08-30 23:11:35 +0100

    Update Admin Commands.py

--- cogs/Admin Commands.py
+++ cogs/Admin Commands.py
@@ -9,7 +9,7 @@ class Admin_Commands(commands.Cog, name='👑 Admin Commands'):
         self.client = client
 
     @commands.has_role(737604230759841792)
-    @commands.command()
+    @commands.command(description='Loads a specified cog.')
     async def load(self, ctx, module : str):
         if ctx.author.id != 221188745414574080:
             pass
@@ -22,7 +22,7 @@ class Admin_Commands(commands.Cog, name='👑 Admin Commands'):
                 await ctx.send('Loaded cog.')
 
     @commands.has_role(737604230759841792)
-    @commands.command()
+    @commands.command(description='Unloads a specified cog.')
     async def unload(self, ctx, module : str):
         if ctx.author.id != 221188745414574080:
             pass
@@ -35,7 +35,7 @@ class Admin_Commands(commands.Cog, name='👑 Admin Commands'):
                 await ctx.send('Unloaded cog.')
 
     @commands.has_role(737604230759841792)
-    @commands.command(aliases=['reload'])
+    @commands.command(aliases=['reload'], description='Reloads a specified cog.')
     async def _reload(self, ctx, module : str):
         if ctx.author.id != 221188745414574080:
             pass
@@ -49,16 +49,7 @@ class Admin_Commands(commands.Cog, name='👑 Admin Commands'):
                 await ctx.send('Successfully reloaded cog.')
 
     @commands.has_role(737604230759841792)
-    @commands.command()
-    async def restart(self, ctx):
-        if ctx.author.id != 221188745414574080:
-            pass
-        else:
-            await ctx.send("Restarting bot...")
-            os.execv('python bot.py')
-
-    @commands.has_role(737604230759841792)
-    @commands.command(aliases=['goodnight', 'jaljjayo', 'sd', 'snowwhendubu', 'maliwhensunoo', 'JaljayoUriKkumsogeseo'])
+    @commands.command(aliases=['goodnight', 'jaljjayo', 'sd', 'snowwhendubu', 'maliwhensunoo', 'JaljayoUriKkumsogeseo'], description='Shuts down the bot.')
     async def shutdown(self, ctx):
         if (ctx.message.author.id != 221188745414574080) and (ctx.message.author.id != 303901339891531779):
             print(f'{datetime.now().strftime("%d/%m/%Y %H:%M:%S")}::: {ctx.message.author} tried to shut the bot down lol')
