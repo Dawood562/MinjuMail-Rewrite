d034ab595cff0bb6d5478f71f88a1c760da8f82d
Dawood <83209168+14685-562@users.noreply.github.com>
2021-08-30 12:47:58 +0100

    Update and rename report.py to Reporting.py

--- cogs/Reporting.py
+++ cogs/Reporting.py
@@ -2,9 +2,9 @@ import discord
 from datetime import datetime
 from discord.ext import commands
 
-class report(commands.Cog):
+class Reporting(commands.Cog):
     def __init__(self, client):
         self.client = client
 
 def setup(client):
-    client.add_cog(report(client))
+    client.add_cog(Reporting(client))
