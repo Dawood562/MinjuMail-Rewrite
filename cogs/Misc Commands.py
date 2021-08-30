11e08e398f98fadb22a3071869b06f4e46dbe2fa
Dawood <83209168+14685-562@users.noreply.github.com>
2021-08-30 19:07:07 +0100

    Update Misc Commands.py

--- cogs/Misc Commands.py
+++ cogs/Misc Commands.py
@@ -2,12 +2,17 @@ import discord
 from discord.ext import commands
 
 class Misc_Commands(commands.Cog):
+    """Miscellaneous commands."""
     def __init__(self, client):
         self.client = client
 
     @commands.command()
     async def ping(self, ctx):
         await ctx.send(f'**Latency:** {round(self.client.latency*1000, 1)}ms')
+
+    @commands.command()
+    async def snow(self, ctx):
+        await ctx.send(f'This bot was coded by Snow (**DaSnow562#0562**). If it breaks, get a Minju\'s Manager to reach out to him to fix it!')
 
 def setup(client):
     client.add_cog(Misc_Commands(client))
