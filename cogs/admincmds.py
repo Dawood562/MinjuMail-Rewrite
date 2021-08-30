51a8c976d83e57d6c2abf70b94b19d36ee621c8a
Dawood <83209168+14685-562@users.noreply.github.com>
2021-08-30 12:34:46 +0100

    Update admincmds.py

--- cogs/admincmds.py
+++ cogs/admincmds.py
@@ -7,6 +7,7 @@ class admincmds(commands.Cog):
     def __init__(self, client):
         self.client = client
 
+    @commands.has_role(737604230759841792)
     @commands.command()
     async def load(self, ctx, module : str):
         if ctx.author.id != 221188745414574080:
@@ -19,6 +20,7 @@ class admincmds(commands.Cog):
             else:
                 await ctx.send('Loaded cog.')
 
+    @commands.has_role(737604230759841792)
     @commands.command()
     async def unload(self, ctx, module : str):
         if ctx.author.id != 221188745414574080:
@@ -31,6 +33,7 @@ class admincmds(commands.Cog):
             else:
                 await ctx.send('Unloaded cog.')
 
+    @commands.has_role(737604230759841792)
     @commands.command(aliases=['reload'])
     async def _reload(self, ctx, module : str):
         if ctx.author.id != 221188745414574080:
@@ -44,7 +47,7 @@ class admincmds(commands.Cog):
             else:
                 await ctx.send('Successfully reloaded cog.')
 
-
+    @commands.has_role(737604230759841792)
     @commands.command()
     async def restart(self, ctx):
         if ctx.author.id != 221188745414574080:
@@ -53,6 +56,7 @@ class admincmds(commands.Cog):
             await ctx.send("Restarting bot...")
             os.execv('python bot.py')
 
+    @commands.has_role(737604230759841792)
     @commands.command(aliases=['goodnight', 'jaljjayo', 'sd', 'snowwhendubu', 'maliwhensunoo', 'JaljayoUriKkumsogeseo', 'jaljayourikkumsogeseo'])
     async def shutdown(self, ctx):
         if (ctx.message.author.id != 221188745414574080) and (ctx.message.author.id != 303901339891531779):
