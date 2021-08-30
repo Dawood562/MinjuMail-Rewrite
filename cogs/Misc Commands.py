fc454fc06453c8436cf162e44c66071b8cc8793e
Dawood <83209168+14685-562@users.noreply.github.com>
2021-08-30 12:46:44 +0100

    Update and rename testcmds.py to Misc Commands.py

--- cogs/Misc Commands.py
+++ cogs/Misc Commands.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-class testcmds(commands.Cog):
+class Misc_Commands(commands.Cog):
     def __init__(self, client):
         self.client = client
 
@@ -10,4 +10,4 @@ class testcmds(commands.Cog):
         await ctx.send(f'**Latency:** {round(self.client.latency*1000, 1)}ms')
 
 def setup(client):
-    client.add_cog(testcmds(client))
+    client.add_cog(Misc_Commands(client))
